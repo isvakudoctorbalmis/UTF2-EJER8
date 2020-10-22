a5741a56acfe2d9a39fa84e46d5db91da773f926
ICMDAW1H <48095884+ICMDAW1H@users.noreply.github.com>
2020-10-22 12:35:19 +0100

    Agregar archivos de proyecto.

---
+++ UTF2-EJER8/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace UTF2_EJER8
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void nombreTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+
+            if (e.Key == Key.F1)
+            { 
+                if (nombreTextBlock.Opacity==0)
+                    nombreTextBlock.Opacity = 1;
+                else             
+                    nombreTextBlock.Opacity = 0;
+            }
+        }
+        private void apellidosTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                if (apellidosTexBlock.Opacity == 0)
+                    apellidosTexBlock.Opacity = 1;
+                else
+                    apellidosTexBlock.Opacity = 0;
+            }
+        }
+        private void edadTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F2)
+            {
+                try
+                {
+                    Convert.ToInt32(edadTextBox.Text);
+                    edadTextBlock.Opacity = 0;
+                }
+                catch
+                {
+                    edadTextBlock.Opacity = 1;
+                }
+            }
+        }
+    }
+}
